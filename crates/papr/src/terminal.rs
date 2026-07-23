99545717d63a070eb6c2b39059634444564b6b53
Saqlain Afroz <sqn3680@gmail.com>
2026-07-23 15:52:39 +0530

    Mouse capture fix

--- crates/papr/src/terminal.rs
+++ crates/papr/src/terminal.rs
@@ -3,6 +3,7 @@
 use std::io::{self, Stdout};
 
 use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
@@ -22,10 +23,16 @@ impl TerminalSession {
             let _ = disable_raw_mode();
             return Err(error);
         }
+        if let Err(error) = execute!(stdout, EnableMouseCapture) {
+            let _ = execute!(stdout, LeaveAlternateScreen);
+            let _ = disable_raw_mode();
+            return Err(error);
+        }
         let terminal = match Terminal::new(CrosstermBackend::new(stdout)) {
             Ok(terminal) => terminal,
             Err(error) => {
                 let mut stdout = io::stdout();
+                let _ = execute!(stdout, DisableMouseCapture);
                 let _ = execute!(stdout, LeaveAlternateScreen);
                 let _ = disable_raw_mode();
                 return Err(error);
@@ -43,8 +50,12 @@ impl TerminalSession {
 impl Drop for TerminalSession {
     fn drop(&mut self) {
         let _ = disable_raw_mode();
+        let _ = execute!(self.terminal.backend_mut(), DisableMouseCapture);
         let _ = execute!(self.terminal.backend_mut(), LeaveAlternateScreen);
-        let _ = execute!(self.terminal.backend_mut(), crossterm::cursor::SetCursorStyle::BlinkingBlock);
+        let _ = execute!(
+            self.terminal.backend_mut(),
+            crossterm::cursor::SetCursorStyle::BlinkingBlock
+        );
         let _ = self.terminal.show_cursor();
     }
 }
